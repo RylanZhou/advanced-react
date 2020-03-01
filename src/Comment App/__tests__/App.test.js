12176c5a771f56d387a5e7de3b6f15a63c573b42
RylanZhou <475777146@qq.com>
2020-03-01 10:52:01 +0800

    Implemented enzyme and refractored testing code in app.test.js.

--- src/Comment App/__tests__/App.test.js
+++ src/Comment App/__tests__/App.test.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-
+import { shallow } from 'enzyme'
 import CommentApp from '../index'
+import CommentBox from '../Components/CommentBox'
+import CommentList from '../Components/CommentList'
 
 // it(a statement, function to prove this statement)
 it('shows a comment box', () => {
-  const div = document.createElement('div')
+  // The component we want to extract. The name "wrapped" is used in most of the documents but its just the component itself.
+  const wrapped = shallow(<CommentApp />)
 
-  ReactDOM.render(<CommentApp />, div)
+  // Test whether there is only one CommentBox inside App
+  expect(wrapped.find(CommentBox).length).toEqual(1)
+})
 
-  // Look inside the virtual div and see whether CommentBox exists
-  expect(div.innerHTML).toContain('Comment Box')
+it('shows a comment list', () => {
+  const wrapped = shallow(<CommentApp />)
 
-  // Destroy memories no longer in use
-  ReactDOM.unmountComponentAtNode(div)
+  expect(wrapped.find(CommentList).length).toEqual(1)
 })
